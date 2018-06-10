1bd8e9dad4430b0300e286006a52b0a2fd0eae71
JippevdMaaden <jippe@vandermaaden.nl>
2018-06-10 13:58:12 +0200

    Update utils.py

--- util/utils.py
+++ util/utils.py
@@ -326,27 +326,27 @@ class CameraCone:
 		rotation = 270
 		tempmatrix = np.matrix([[1,0,0,0],[0,np.cos(np.radians(rotation)),-np.sin(np.radians(-rotation)),0],[0,np.sin(np.radians(rotation)),np.cos(np.radians(rotation)),0],[0,0,0,1]])
 		self.rotationmatrix = tempmatrix.I
-		xaxis = tempmatrix.I
+		xaxis = tempmatrix
 		print xaxis
 		
 		#rotate the camera 90 deg along the y-axis (look east in stead of down)
 		rotation = 0
 		tempmatrix = np.matrix([[np.cos(np.radians(rotation)),0,np.sin(np.radians(rotation)),0],[0,1,0,0],[-np.sin(np.radians(rotation)),0,np.cos(np.radians(rotation)),0],[0,0,0,1]])
 		self.rotationmatrix = tempmatrix.I
-		yaxis = tempmatrix.I
+		yaxis = tempmatrix
 		print yaxis
 		
 		#rotate the camera 90 deg along the z-axis (still look down, make west become north)
 		rotation = 0
 		tempmatrix = np.matrix([[np.cos(np.radians(rotation)),-np.sin(np.radians(rotation)),0,0],[np.sin(np.radians(rotation)),np.cos(np.radians(rotation)),0,0],[0,0,1,0],[0,0,0,1]])
 		self.rotationmatrix = tempmatrix.I
-		zaxis = tempmatrix.I
+		zaxis = tempmatrix
 		print zaxis
 		
 		allaxis = xaxis.dot(yaxis.dot(zaxis))
 		print allaxis
 		
-		self.rotationmatrix = allaxis
+		self.rotationmatrix = allaxis.I
 		
 		# adjust for possible rectangular resolution
 		
