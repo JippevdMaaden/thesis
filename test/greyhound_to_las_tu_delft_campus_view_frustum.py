22bcdd71fe772ae3148a60c5a89540692e724b08
JippevdMaaden <jippe@vandermaaden.nl>
2018-06-11 14:57:42 +0200

    Update greyhound_to_las_tu_delft_campus_view_frustum.py

--- test/greyhound_to_las_tu_delft_campus_view_frustum.py
+++ test/greyhound_to_las_tu_delft_campus_view_frustum.py
@@ -174,8 +174,8 @@ if __name__ == '__main__':
     goodpoints = []
     
     # override raw_input for testing
-    cameraorigin = (85910, 445600, 0)
-    cameratarget = (85910, 445600, -1)
+    cameraorigin = (85910, 445600, 500)
+    cameratarget = (85910, 445600, -500)
     viewfrustum = CameraCone(_3Dcenter, cameraorigin, cameratarget, fov)
     
     inFile = openLasFile('originalfile.las')
