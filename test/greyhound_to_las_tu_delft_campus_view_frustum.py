78e75648ed8c5cb66aa2d5a21102703b7fe08e39
JippevdMaaden <jippe@vandermaaden.nl>
2018-06-11 15:07:05 +0200

    Update greyhound_to_las_tu_delft_campus_view_frustum.py

--- test/greyhound_to_las_tu_delft_campus_view_frustum.py
+++ test/greyhound_to_las_tu_delft_campus_view_frustum.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     goodpoints = []
     
     # override raw_input for testing
-    cameraorigin = (85910, 445600, 1)
+    cameraorigin = (85910, 445600, 10)
     cameratarget = (85910, 445600, -500)
     viewfrustum = CameraCone(_3Dcenter, cameraorigin, cameratarget, fov)
     
