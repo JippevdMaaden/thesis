09625d854bc63202b18c4425de75485e112fae4e
JippevdMaaden <jippe@vandermaaden.nl>
2018-06-19 14:21:05 +0200

    Update vario_scale_potree_diam_5.py

--- test/vario_scale_potree_diam_5.py
+++ test/vario_scale_potree_diam_5.py
@@ -184,7 +184,7 @@ if __name__ == '__main__':
       distance = (distancevector[0] ** 2 + distancevector[1] ** 2 + distancevector[2] ** 2) ** 0.5
       
       # implement logarithmic(e) function here
-      nn = kdtree.query_ball_point(point, distance * 0.01)
+      nn = kdtree.query_ball_point(point, np.log(distance))
       
       appendvar = True
       
