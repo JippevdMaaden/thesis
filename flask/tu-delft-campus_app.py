eb1779dea33ce40101f4e6835844494d0a9e65cf
JippevdMaaden <jippe@vandermaaden.nl>
2018-07-27 12:03:19 +0200

    Update tu-delft-campus_app.py

--- flask/tu-delft-campus_app.py
+++ flask/tu-delft-campus_app.py
@@ -61,6 +61,7 @@ class Greyhound_read(Resource):
     greyhound_server = getGreyhoundServer()
     server_to_call = '{}{}/read?{}'.format(greyhound_server[:-1], prefix_resource, string_to_add)
     
+    print server_to_call
     #return read(server_to_call)
     return server_to_call
     return temp_dict
