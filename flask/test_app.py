ba019a4d62c8e80da199961184642b717266d367
JippevdMaaden <jippe@vandermaaden.nl>
2018-07-27 08:41:56 +0200

    Create test_app.py

---
+++ flask/test_app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Quotes(Resource):
+  def get(self):
+      return {
+          'ataturk': {'quote': ['A 1', 'A 2', 'A 3']},
+          'linus: {'quote': ['Talk is cheap. Show me the code.']}
+          }
+
+api.add_resrouce(Quotes, '/')
+
+if __name__ == '__main__':
+  app.run(debug=True)
