c6a6321b70bd144cb1d1c6a94fd23cb787d37d23
JippevdMaaden <jippe@vandermaaden.nl>
2018-07-27 11:08:21 +0200

    Update tu-delft-campus_app.py

--- flask/tu-delft-campus_app.py
+++ flask/tu-delft-campus_app.py
@@ -1,7 +1,7 @@
 import sys
 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 sys.path.insert(0, '/home/ec2-user/thesis')
 
@@ -15,15 +15,22 @@ greyhoud_server = getGreyhoundServer()
 
 class Greyhound_read(Resource):
   def get(self, action):
+    parser = reqparse.RequestParser()
+    parser.add_argument('depthEnd', type=int)
+    parser.add_argument('depthBegin', type=int)
+    
+    return parser.parse_args()
+    
     greyhound_server = getGreyhoundServer()
     server_to_call = greyhound_server + action
+    
     return 'im reading this, will forward it to {}'.format(server_to_call)
 
 class Greyhound_info(Resource):
   def get(self):
     return 'im reading this info'
 
-api.add_resource(Greyhound_read, prefix_resource + '/read<string:action>')
+api.add_resource(Greyhound_read, prefix_resource + '/read', endpoint='read')
 api.add_resource(Greyhound_info, prefix_resource + '/info')
 
 if __name__ == '__main__':
