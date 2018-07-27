c5a59ec488f4c05295ffb2f1aa8d3345cc4c37d9
JippevdMaaden <jippe@vandermaaden.nl>
2018-07-27 11:34:33 +0200

    Update tu-delft-campus_app.py

--- flask/tu-delft-campus_app.py
+++ flask/tu-delft-campus_app.py
@@ -21,7 +21,7 @@ class Greyhound_read(Resource):
     parser.add_argument('depthBegin', type=int)
     parser.add_argument('bounds', type=str)
     parser.add_argument('scale', type=float)
-    parser.add_argument('offset', type=list)
+    parser.add_argument('offset', type=str)
     parser.add_argument('filter')
     parser.add_argument('schema')
     parser.add_argument('compress', type=str)
