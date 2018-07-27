1e1b9756a0f90eecb20698e254db6d24007928a0
JippevdMaaden <jippe@vandermaaden.nl>
2018-07-27 10:49:13 +0200

    Update tu-delft-campus_app.py

--- flask/tu-delft-campus_app.py
+++ flask/tu-delft-campus_app.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+from util.utils import *
+
 app = Flask(__name__)
 api = Api(app)
 
 prefix_resource = '/resource/tu-delft-campus'
+greyhoud_server = getGreyhoundServer()
 
 class Greyhound_read(Resource):
-  def get(self):
-    return 'im reading this'
+  def get(self, action):
+    server_to_call = greyhound_server + action
+    return 'im reading this, will forward it to {}'.format(server_to_call)
 
 class Greyhound_info(Resource):
   def get(self):
     return 'im reading this info'
 
-api.add_resource(Greyhound_read, prefix_resource + '/read')
+api.add_resource(Greyhound_read, prefix_resource + '/read<string:action>')
 api.add_resource(Greyhound_info, prefix_resource + '/info')
 
 if __name__ == '__main__':
