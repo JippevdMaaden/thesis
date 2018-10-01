39af3acc84a4e01b8c7ab0e921efb4ec28ab59e3
JippevdMaaden <jippe@vandermaaden.nl>
2018-10-01 10:25:05 +0200

    Create class for writing pointclouds

--- util/utils.py
+++ util/utils.py
@@ -426,3 +426,93 @@ class CameraCone:
 				return False
 
 		return True
+
+	
+class GreyhoundConnection():
+	def __init__(self, resource, param_schema, filename):
+	    self.base_url = getGreyhoundServer() + 'resource/'
+	    self.resource = resource	    
+	    self.j = self.info()
+	    self.filename = filename
+	
+	def get_pointcloud(self, param_schema):
+	    self.param_schema = param_schema
+	    self.param_str = ''
+	    
+	    for param in self.param_schema:
+		self.param_str += '{}={}'.format(str(param), str(self.param_schema[param]))
+		self.param_str += '&'
+	    
+	    self.param_str = self.param_str[:-1]
+		
+	    j = self.info()
+	    data = self.read()
+	    writeLASfile(data, self.filename)
+
+	def info(self):
+	    url = self.base_url + self.resource + '/info'
+	    u = urlopen(url)
+	    data = u.read()
+	    j = json.loads(data)
+	    j['dtype'] = self.buildNumpyDescription(j['schema'])
+	    return j
+	
+	def read(self):
+	    url = self.base_url + self.resource + '/read?' + self.param_str
+	    u = urlopen(url)
+	    data = u.read()
+	    count = struct.unpack('<L',data[-4:])[0]
+	    array = np.ndarray(shape=(count,),buffer=data,dtype=self.info['dtype'])
+	    return array
+
+	def buildNumpyDescription(self, schema):
+            output = {}
+            formats = []
+            names = []
+            for s in schema:
+                t = s['type']
+                if t == 'floating':
+                    t = 'f'
+                elif t == 'unsigned':
+                    t = 'u'
+                else:
+                    t = 'i'
+
+                f = '%s%d' % (t, int(s['size']))
+                names.append(s['name'])
+                formats.append(f)
+            output['formats'] = formats
+            output['names'] = names
+            return output
+
+	def writeLASfile(self, data, filename):
+	    try:
+		minx = min(d['X'])
+		miny = min(d['Y'])
+		minz = min(d['Z'])
+	    except ValueError:
+		print 'No points available in this bbox geometry'
+		return
+
+	    header = laspy.header.Header()
+	    scale = 0.01
+	    header.x_scale = scale; header.y_scale = scale; header.z_scale = scale
+	    header.x_offset = minx ;header.y_offset = miny; header.z_offset = minz
+	    header.offset = [minx, miny, minz]
+
+	    X = (d['X'] - header.x_offset)/header.x_scale
+	    Y = (d['Y'] - header.y_offset)/header.y_scale
+	    Z = (d['Z'] - header.z_offset)/header.z_scale
+	    output = laspy.file.File(filename, mode='w', header = header)
+	    output.X = X
+	    output.set_scan_dir_flag(d['ScanDirectionFlag'])
+	    output.set_intensity(d['Intensity'])
+	    output.set_scan_angle_rank(d['ScanAngleRank'])
+	    output.set_pt_src_id(d['PointSourceId'])
+	    output.set_edge_flight_line(d['EdgeOfFlightLine'])
+	    output.set_return_num(d['ReturnNumber'])
+	    output.set_num_returns(d['NumberOfReturns'])
+	    output.Y = Y
+	    output.Z = Z
+	    output.Raw_Classification = d['Classification']
+	    output.close()
